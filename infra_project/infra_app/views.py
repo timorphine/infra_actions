431bca3deaf6c13798f02cff20037a075dc8b4c7
Timofey Kondakov <tkondakov98@yandex.ru>
2023-02-07 16:21:14 +0400

    change code

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Ура, все работает!')
 
 
 def second_page(request):
